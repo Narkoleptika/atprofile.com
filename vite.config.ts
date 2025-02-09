c14e25b0537f99b90a3f8a33e12e3c135e36675d
Joey Marsh <joe@joey.codes>
2025-02-08 22:13:22 -0800

    add csp

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-    plugins: [vue(), vueDevTools(), tailwindcss()],
+    plugins: [
+        vue(),
+        vueDevTools(),
+        tailwindcss(),
+        {
+            name: 'dev-unsafe-inline-style',
+            transformIndexHtml(html) {
+                if (mode === 'production') {
+                    return
+                }
+
+                return html.replace(/style-src([^;]+);/m, "style-src$1 'unsafe-inline';")
+            },
+        },
+    ],
     server: {
         host: '0.0.0.0',
         cors: true,
