f0da65d660151db549600aab19aefcf4eb77c489
Joey Marsh <joe@joey.codes>
2025-03-04 00:10:15 -0800

    handle licenses w/o license text

--- src/views/LicenseView.vue
+++ src/views/LicenseView.vue
@@ -4,12 +4,14 @@ const licenses = ref([
     {
         name: 'package 1',
         version: '1.0.0',
+        license: 'MIT',
         licenseText:
             'The MIT License (MIT)\n\nCopyright (c) 1-present, Human Person\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in\nall copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\nTHE SOFTWARE.\n',
     },
     {
         name: 'package 2',
         version: '0.0.1',
+        license: 'MIT',
         licenseText:
             'The MIT License (MIT)\n\nCopyright (c) 1-present, Human Person\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in\nall copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\nTHE SOFTWARE.\n',
     },
@@ -34,9 +36,9 @@ onBeforeMount(async () => {
         <div class="prose m-auto">
             <h1 class="text-center mb-0">Acknowledgements</h1>
             <ul class="list bg-base-200 rounded-box shadow-md mt-2 mb-2">
-                <li v-for="{ name, version, licenseText } in licenses" :key="name" class="list-row">
-                    <h2 class="text-center mt-0">{{ name }} {{ version }}</h2>
-                    <div class="list-col-wrap whitespace-break-spaces">{{ licenseText }}</div>
+                <li v-for="{ name, version, license, licenseText } in licenses" :key="name" class="list-row">
+                    <h2 class="text-center mt-0 col-start-1 -col-end-1">{{ name }} {{ version }}</h2>
+                    <div class="list-col-wrap whitespace-break-spaces">{{ licenseText || `License: ${license}` }}</div>
                 </li>
             </ul>
         </div>
